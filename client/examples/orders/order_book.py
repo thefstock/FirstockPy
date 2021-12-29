d0a87d050822ca3ea1294456b21f8e4f09c6f9cd
vikasetrx <3935644+vikasetrx@users.noreply.github.com>
2021-12-30 01:12:16 +1300

    Add Order Book example

---
+++ client/examples/orders/order_book.py
@@ -0,0 +1,34 @@
+"""
+Example usage of logout request
+"""
+from py_client import OrderBookRequestModel
+from argparse import ArgumentParser
+
+from ..base import Example
+
+class OrderBookRequestExample(Example):
+
+  title: str = 'Order Book'
+
+  def parse_args(self):
+    """
+    Parse CLI arguments
+    """
+    parser = ArgumentParser('examples.orders.order_book')
+    parser.add_argument('--uid', type=str, help='The user id')
+    parser.add_argument('--token', type=str, help='The token recieved from login')
+    self.args = parser.parse_args()
+
+  def run(self):
+    """
+    Run the example
+    """
+    model = OrderBookRequestModel(
+      uid=self.args.uid
+    )
+    response = self.client.orders.order_book(model, key=self.args.token)
+    return [model, response]
+
+# Run example
+example = OrderBookRequestExample()
+example()
